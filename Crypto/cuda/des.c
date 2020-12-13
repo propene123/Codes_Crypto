eb9b9363d3e4d9c3fd1db97a301451e1cabf01ef
Propene123 <benjamin.russell@durham.ac.uk>
2020-12-13 16:29:59 +0000

    start encrpyt

--- Crypto/cuda/des.c
+++ Crypto/cuda/des.c
@@ -1,5 +1,5 @@
-#include <stdint.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 
 static uint32_t lrot32(uint32_t in, uint8_t n) {
@@ -13,31 +13,38 @@ static uint64_t cat32(uint32_t left, uint32_t right) {
 
 static uint64_t buff_to_int(unsigned char buff[8]) {
 
-    uint64_t res = 0;
-    for(int i = 0;i<8;++i){
-        res |= ((uint64_t)buff[i]) << (CHAR_BIT * (7-i));
-    }
-    return res;
+  uint64_t res = 0;
+  for (int i = 0; i < 8; ++i) {
+    res |= ((uint64_t)buff[i]) << (CHAR_BIT * (7 - i));
+  }
+  return res;
+}
+
+static uint64_t perm(const uint64_t in, const size_t in_length,
+                     const uint8_t *table, const size_t out_length) {
+  uint64_t out = 0;
+  for (size_t i = 0; i < out_length; ++i) {
+    out <<= 1ul;
+    if (in & (1ul << (in_length - table[i])))
+      out |= 1ul;
+  }
+  return out;
 }
 
 void gen_sched(unsigned char key[8], uint64_t sched[16]) {
 
-  uint8_t pc1[56] = {57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34, 26, 18,
-                     10, 2,  59, 51, 43, 35, 27, 19, 11, 3,  60, 52, 44, 36,
-                     63, 55, 47, 39, 31, 23, 15, 7,  62, 54, 46, 38, 30, 22,
-                     14, 6,  61, 53, 45, 37, 29, 21, 13, 5,  28, 20, 12, 4};
+  const uint8_t pc1[56] = {57, 49, 41, 33, 25, 17, 9,  1,  58, 50, 42, 34,
+                           26, 18, 10, 2,  59, 51, 43, 35, 27, 19, 11, 3,
+                           60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7,
+                           62, 54, 46, 38, 30, 22, 14, 6,  61, 53, 45, 37,
+                           29, 21, 13, 5,  28, 20, 12, 4};
 
-  uint8_t pc2[48] = {14, 17, 11, 24, 1,  5,  3,  28, 15, 6,  21, 10,
-                     23, 19, 12, 4,  26, 8,  16, 7,  27, 20, 13, 2,
-                     41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-                     44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
+  const uint8_t pc2[48] = {14, 17, 11, 24, 1,  5,  3,  28, 15, 6,  21, 10,
+                           23, 19, 12, 4,  26, 8,  16, 7,  27, 20, 13, 2,
+                           41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+                           44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
   uint64_t key_int = buff_to_int(key);
-  uint64_t key_perm = 0;
-  for (int i = 0; i < 56; ++i) {
-    key_perm <<= 1;
-    if (key_int & (1ul << (64-pc1[i])))
-      key_perm |= 1ul;
-  }
+  uint64_t key_perm = perm(key_int, 64, pc1, 56);
   uint32_t key_left = key_perm >> 28;
   uint32_t key_right = 0x000000000fffffff & key_perm;
   uint32_t c = lrot32(key_left, 1);
@@ -55,12 +62,20 @@ void gen_sched(unsigned char key[8], uint64_t sched[16]) {
     }
   }
   for (int i = 0; i < 16; ++i) {
-    key_perm = 0;
-    for (int j = 0; j < 48; ++j) {
-      key_perm <<= 1;
-      if (sched[i] & (1ul << (56-pc2[j])))
-        key_perm |= 1ul;
-    }
+    key_perm = perm(sched[i], 56, pc2, 48);
     sched[i] = key_perm;
   }
+}
+
+static const uint8_t ip[64] = {
+    58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+    57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+
+void encrypt(unsigned char in[8], unsigned char out[8], uint64_t sched[16]) {
+  uint64_t in_int = buff_to_int(in);
+  uint64_t in_perm = perm(in_int, 64, ip, 64);
+  uint32_t left = in_perm >> 32;
+  uint32_t right = in_perm & 0xffffffff;
 }
--- Crypto/cuda/des.h
+++ Crypto/cuda/des.h
@@ -3,5 +3,6 @@
 #include <stdint.h>
 
 void gen_sched(unsigned char key[8], uint64_t sched[16]);
+void encrypt(unsigned char in[8], unsigned char out[8], uint64_t sched[16]);
 
 #endif 
