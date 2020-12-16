5e5537c9dd6a54434683c0b84bcd826d241ac87b
Propene123 <benjamin.russell@durham.ac.uk>
2020-12-16 02:44:27 +0000

    meh

--- Crypto/cuda/des.c
+++ Crypto/cuda/des.c
@@ -8,8 +8,12 @@ static uint32_t lrot32(uint32_t in, uint8_t n) {
     return (ret & 0x0fffffff);
 }
 
-static uint64_t cat32(const uint32_t left, const uint32_t right) {
+static uint64_t cat28(const uint32_t left, const uint32_t right) {
     return (((uint64_t)left) << 28) | ((uint64_t)right);
+}
+
+static uint64_t cat32(const uint32_t left, const uint32_t right) {
+    return (((uint64_t)left) << 32) | ((uint64_t)right);
 }
 
 static uint64_t buff_to_int(const unsigned char buff[8]) {
@@ -22,7 +26,7 @@ static uint64_t buff_to_int(const unsigned char buff[8]) {
 
 static void int_to_buff(const uint64_t in, unsigned char out[8]) {
     for (int i = 0; i < 8; ++i) {
-        out[i] = (unsigned char)((in >> (7 - i)) & 0xff);
+        out[i] = (unsigned char)((in >> (CHAR_BIT * (7 - i))) & 0xff);
     }
 }
 
@@ -41,11 +45,11 @@ static uint32_t read_s_block(const uint8_t in, const uint8_t s_block[4][16],
                              const int id) {
     const uint8_t row = (in & 1) | ((in & 0x20) >> 4);
     const uint8_t col = (in & 0x1e) >> 1;
-    return ((uint32_t)s_block[row][col]) << (CHAR_BIT * (7 - id));
+    return ((uint32_t)s_block[row][col]) << (4 * (8 - id));
 }
 
 static uint8_t get_mini_block(const uint64_t in, const int id) {
-    return ((in >> (CHAR_BIT * (8 - id))) & 0x3f);
+    return ((in >> (6 * (8 - id))) & 0x3f);
 }
 
 static uint32_t feistel(const uint32_t in, const uint64_t key) {
@@ -70,16 +74,16 @@ void gen_sched(unsigned char key[8], uint64_t sched[16]) {
     uint32_t key_right = 0x000000000fffffff & key_perm;
     uint32_t c = lrot32(key_left, 1);
     uint32_t d = lrot32(key_right, 1);
-    sched[0] = cat32(c, d);
+    sched[0] = cat28(c, d);
     for (int i = 0; i < 15; ++i) {
         if (i % 7 == 0) {
             c = lrot32(c, 1);
             d = lrot32(d, 1);
-            sched[i + 1] = cat32(c, d);
+            sched[i + 1] = cat28(c, d);
         } else {
             c = lrot32(c, 2);
             d = lrot32(d, 2);
-            sched[i + 1] = cat32(c, d);
+            sched[i + 1] = cat28(c, d);
         }
     }
     for (int i = 0; i < 16; ++i) {
--- Crypto/cuda/des_test.c
+++ Crypto/cuda/des_test.c
@@ -8,7 +8,7 @@ int main() {
     unsigned char buff[8] = {0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1};
     uint64_t sched[16];
     gen_sched(buff, sched);
-    unsigned char in[8] = "abcdefgh";
+    unsigned char in[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
     unsigned char out[8];
     encrypt(in, out, sched);
     printf("0x");
