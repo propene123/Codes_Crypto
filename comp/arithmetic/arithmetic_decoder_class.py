d655a04b4ecf5a42a3831d20672e6f672aa43148
Propene123 <benjamin.russell@durham.ac.uk>
2021-01-11 21:11:16 +0000

    moar

--- comp/arithmetic/arithmetic_decoder_class.py
+++ comp/arithmetic/arithmetic_decoder_class.py
@@ -6,5 +6,23 @@ class ArithmeticDecoder():
     def __init__(self, symbol_size, symbol_num, init_dict):
         self.table = init_dict
         self.symbol_size = symbol_size
-        self.symbol_num = symbol_num
         getcontext().prec = 999
+
+
+    def decode(self, in_stream):
+        low = 000000000
+        high = 999999999
+        code = in_stream.read('uint:32')
+        index = ((code - low)*self.symbol_num-1)/(high-low+1)
+        index = int(index)
+
+
+
+
+in_bytes = bytearray()
+with open('out.lz', 'rb') as f:
+    in_bytes = bytearray(f.read())
+
+in_stream = BitStream(in_bytes)
+decoder = ArithmeticDecoder(8, dict())
+decoder.decode(in_stream)
